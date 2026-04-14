5658a58e13c649ff9cc19f2bfe3642af36804f10
moajebli <64327620+moajebli@users.noreply.github.com>
2026-04-14 08:27:14 +0000

    restore files

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import * as core from "@actions/core";
 async function run() {
   const joke = await getJoke();
   console.log(joke);
-  core.setOutput("Joke", joke);
+  core.setOutput("joke", joke);
 }
 
 run();
