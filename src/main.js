3a26c8fb32d95d40235bbc64103fd85f82a71614
moajebli <64327620+moajebli@users.noreply.github.com>
2026-04-14 08:28:34 +0000

    new changes

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import * as core from "@actions/core";
 async function run() {
   const joke = await getJoke();
   console.log(joke);
-  core.setOutput("joke", joke);
+  core.setOutput("Joke", joke);
 }
 
 run();
